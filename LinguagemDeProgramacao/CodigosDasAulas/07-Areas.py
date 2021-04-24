4efe1dbc6348d9edebcbd25ff4887b2a9fd6f0ab
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-04-23 22:32:36 -0300

    Update 07-Areas.py
    Atualiza o código corrigindo o erro de formatação

--- LinguagemDeProgramacao/CodigosDasAulas/07-Areas.py
+++ LinguagemDeProgramacao/CodigosDasAulas/07-Areas.py
@@ -7,8 +7,8 @@ circulo = 3.14159*C**2
 trapezio=((A+B)*C)/2
 quadrado=B**2
 retangulo=A*B
-print('TRIANGULO: {:.3f}', .format(triangulo))
-print('CIRCULO: {:.3f}', .format(circulo))
-print('TRAPEZIO: {:.3f}', .format(trapezio))
-print('QUADRADO: {:.3f}', .format(quadrado))
-print('RETANGULO: {:.3f}', .format(retangulo))
+print('TRIANGULO: {:.3f}'.format(triangulo))
+print('CIRCULO: {:.3f}'.format(circulo))
+print('TRAPEZIO: {:.3f}'.format(trapezio))
+print('QUADRADO: {:.3f}'.format(quadrado))
+print('RETANGULO: {:.3f}'.format(retangulo))
