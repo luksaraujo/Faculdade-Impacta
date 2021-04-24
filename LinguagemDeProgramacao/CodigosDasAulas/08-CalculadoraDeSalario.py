81fd0122675d983c00bf39ba015c1396a96798db
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-04-23 22:42:11 -0300

    Create 08-CalculadoraDeSalario.py
    Cria o arquivo que lê três valores (número do funcionário (int), quantidade de horas trabalhadas (int) e valor recebido por hora trabalhada (float)), calcula o salário do funcionário e o exibe na tela.

---
+++ LinguagemDeProgramacao/CodigosDasAulas/08-CalculadoraDeSalario.py
@@ -0,0 +1,7 @@
+numeroFunc = int(input())
+qtdeHorasTrab = int(input())
+valorHora = float(input())
+salario = valorHora * qtdeHorasTrab
+
+print(f'NUMBER = {numeroFunc}')
+print('SALARY = U$ {:.2f}'.format(salario))
