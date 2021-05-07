b7317983c5469ca66f1b2a19f39962059a70ea55
Lucas Araújo <79344483+luksaraujo@users.noreply.github.com>
2021-05-07 20:05:37 -0300

    Create 20-TipoDeAnimal.py
    Cria um código que lê três classificações de animais e exibe qual é o tipo de animal baseado nas entradas do usuário.

---
+++ LinguagemDeProgramacao/CodigosDasAulas/20-TipoDeAnimal.py
@@ -0,0 +1,30 @@
+animal = input()
+
+if animal == "vertebrado":
+    animal=input()
+    if animal == "ave":
+        animal=input()
+        if animal == "carnivoro":
+            print('aguia')
+        else:
+            print('pomba')
+    else:
+        animal=input()
+        if animal == "onivoro":
+            print('homem')
+        else:
+            print('vaca')
+else:
+    animal=input()
+    if animal == "inseto":
+        animal=input()
+        if animal == "hematofago":
+            print('pulga')
+        else:
+            print('lagarta')
+    else:
+        animal = input()
+        if animal == "hematofago":
+            print('sanguessuga')
+        else:
+            print('minhoca')
