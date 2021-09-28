efe4f794cb937445a58105eef4ec94531c62e499
Lucas Araujo <lucasarasouza@hotmail.com>
2021-09-27 21:49:30 -0300

    Adiciona o terceiro exercício do URI e as informações sobre ele no arquivo README.md

---
+++ Técnicas de Programação/Exercícios do URI/3_LED.py
@@ -0,0 +1,40 @@
+# João quer montar um painel de leds contendo diversos números. Ele não possui muitos leds, e não tem certeza se conseguirá
+# montar o número desejado. Considerando a configuração dos leds dos números abaixo, faça um algoritmo que ajude João a descobrir
+# a quantidade de leds necessário para montar o valor.
+#
+# Entrada
+# A entrada contém um inteiro N, (1 ≤ N ≤ 1000) correspondente ao número de casos de teste, seguido de N linhas, cada linha contendo
+# um número (1 ≤ V ≤ 10100) correspondente ao valor que João quer montar com os leds.
+#
+# Saída
+# Para cada caso de teste, imprima uma linha contendo o número de leds que João precisa para montar o valor desejado, seguido da palavra "leds".
+
+def qtt_leds(number):
+    if number == "1":
+        return 2
+    elif number == "2":
+        return 5
+    elif number == "3":
+        return 5
+    elif number == "4":
+        return 4
+    elif number == "5":
+        return 5
+    elif number == "6":
+        return 6
+    elif number == "7":
+        return 3
+    elif number == "8":
+        return 7
+    elif number == "9":
+        return 6
+    else:
+        return 6
+
+qtt_tests = int(input())
+for i in range(qtt_tests):
+    value = input()
+    somador = 0
+    for j in value:
+        somador += qtt_leds(j)
+    print(f'{somador} leds')
--- Técnicas de Programação/Exercícios do URI/README.md
+++ Técnicas de Programação/Exercícios do URI/README.md
@@ -10,3 +10,5 @@ Este diretório armazena todos os códigos que foram desenvolvidos na linguagem 
 - - Lê um valor referente à quantidade de testes que serão executados e, em seguida, lê um número grande e um número menor e diz se o número menor está contido nos últimos números do número grande. A separação dos números é feita por espaço.
 - **`2_O_Jogo_Matematico_de_Paula.py`**
 - - Programa que recebe um número, uma letra e outro número na mesma linha sem separação. De acordo com a letra e com os números, um resultado diferente é exibido.
+- **`3_LED.py`**
+- - Programa que recebe um número inteiro que representa a quantidade de casos de teste e, em seguida, um número. O programa diz quantos leds são necessários para criar o número de acordo com o padrão digital de exibição de números.
