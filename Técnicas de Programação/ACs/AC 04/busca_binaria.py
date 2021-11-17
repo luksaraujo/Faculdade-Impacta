b20dea369787425e3989c70d4670243f9e476b1d
Lucas Araújo <lucasarasouza@hotmail.com>
2021-11-17 12:28:02 -0300

    Atualiza o algoritmo de busca binária inserindo uma função que ordena a lista

--- Técnicas de Programação/ACs/AC 04/busca_binaria.py
+++ Técnicas de Programação/ACs/AC 04/busca_binaria.py
@@ -1,4 +1,7 @@
 def busca_binaria(v, x):
+    print(v)
+    bubble_sort(v)
+    print(v)
     ini = 0
     fim = len(v) - 1
     while ini <= fim:
@@ -13,5 +16,17 @@ def busca_binaria(v, x):
             fim = meio - 1
     return -1 # Não encontrou
 
+def bubble_sort(list_to_sort):
+    elements = len(list_to_sort) - 1
+    is_list_sorted = False
+    while not is_list_sorted:
+        is_list_sorted = True
+        for i in range(elements):
+            if list_to_sort[i] > list_to_sort[i + 1]:
+                list_to_sort[i], list_to_sort[i + 1] = list_to_sort[i + 1], list_to_sort[i]
+                is_list_sorted = False
+    return list_to_sort
+
 # lista = [1,5,18,15,25,50,100]
-# print(busca_binaria(lista, 18))
+# lista = [3,10,45,75,78,95,105,110]
+# print(busca_binaria(lista, 46))
